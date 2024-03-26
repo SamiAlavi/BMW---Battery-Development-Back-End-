0c5b49fed18db997d0871f225195d2125e1b37fb
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2024-03-26 02:05:26 +0100

    DatabaseService

---
+++ services/databaseservice.ts
@@ -0,0 +1,80 @@
+import sqlite3 from 'sqlite3';
+require('dotenv').config();
+
+const sqlite3Verbose = sqlite3.verbose()
+const dbPath = process.env.DB_NAME ?? "database.db";
+
+class DatabaseService {
+    private db = new sqlite3.Database(dbPath);
+
+    constructor() {
+    }
+
+    public close(): void {
+        this.db.close((err) => {
+            if (err) {
+                console.error('Error closing database connection:', err.message);
+                return;
+            }
+            console.log('Database connection closed.');
+        });
+    }
+
+    public query(sql: string, params: any[] = []): Promise<any[]> {
+        return new Promise((resolve, reject) => {
+            this.db.all(sql, params, (err, rows) => {
+                if (err) {
+                    console.error('Error executing query:', err.message);
+                    reject(err);
+                    return;
+                }
+                resolve(rows);
+            });
+        });
+    }
+
+    public insert(sql: string, params: any[] = []): Promise<number> {
+        return new Promise((resolve, reject) => {
+            this.db.run(sql, params, function(err) {
+                if (err) {
+                    console.error('Error executing insert:', err.message);
+                    reject(err);
+                    return;
+                }
+                resolve(this.lastID);
+            });
+        });
+    }
+
+    public update(sql: string, params: any[] = []): Promise<number> {
+        return new Promise((resolve, reject) => {
+            this.db.run(sql, params, function(err) {
+                if (err) {
+                    console.error('Error executing update:', err.message);
+                    reject(err);
+                    return;
+                }
+                resolve(this.changes);
+            });
+        });
+    }
+
+    public delete(sql: string, params: any[] = []): Promise<number> {
+        return new Promise((resolve, reject) => {
+            this.db.run(sql, params, function(err) {
+                if (err) {
+                    console.error('Error executing delete:', err.message);
+                    reject(err);
+                    return;
+                }
+                resolve(this.changes);
+            });
+        });
+    }
+}
+
+const databaseService = new DatabaseService()
+
+export {
+    databaseService,
+}
