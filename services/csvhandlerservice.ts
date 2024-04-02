0e6f365fa9da6763686bc0b9b49642ca2f249aba
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2024-04-03 01:27:46 +0200

    get headers

--- services/csvhandlerservice.ts
+++ services/csvhandlerservice.ts
@@ -6,6 +6,10 @@ class CSVHandlerService {
     private readonly table_CSV_Data = "CSV_Data"
     private readonly table_Capacity = "capacity"
     private readonly table_Cycle = "cycle"
+
+    private readonly cols_CSV_Data = ['filename', 'type', 'timestamp']
+    private readonly cols_Capacity = ['file_id', 'cycle_number', 'capacity']
+    private readonly cols_Cycle = ['file_id', 'cycle_number', 'time', 'current', 'voltage']
     
     private readonly sql_CSV_Data = `INSERT INTO ${this.table_CSV_Data} (filename, type, timestamp) VALUES (?, ?, ?) `
     private readonly sql_Capacity = `INSERT INTO ${this.table_Capacity} (file_id, cycle_number, capacity) VALUES `
@@ -16,11 +20,15 @@ class CSVHandlerService {
 
         fs.createReadStream(filepath)
           .pipe(csv())
+          .on('headers', (headers) => {
+          })
           .on('data', (data) => fileRows.push(data))
           .on('end', () => {
+
+            setTimeout(() => {
+                fs.unlink(filepath, ()=>{})
+            }, 1000)
           });
-  
-          fs.unlink(filepath, ()=>{})
     }
 
 }
